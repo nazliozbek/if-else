7d36d6fb1a8abfcf482427934ce445f8d56314e9
nazliozbek <102694546+nazliozbek@users.noreply.github.com>
2022-04-01 22:07:56 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace if_else
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time= DateTime.Now.Hour;
+            
+            if(time>=6 && time<=11)
+            {
+                Console.WriteLine("Gunaydin");
+            }
+             else if(time<=18)
+             {
+                 Console.WriteLine("iyi gunler");
+             }
+             else
+             {
+                 Console.WriteLine("iyi geceler");
+             }
+
+             string sonuc = time <=18 ? "iyi gunler" : "iyi geceler";
+             sonuc = time>=6 && time<=11 ? "gunaydin" : time<=18 ? "iyi gunler" : "iyi geceler";
+
+             Console.WriteLine(sonuc);
+        }
+    }
+}
